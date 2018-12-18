0dc300d4165c606b1116c87465d1dd32f484991c
kamihati <dream928@gmail.com>
2018-12-18 14:33:52 +0800

    update file

--- cate.py
+++ cate.py
@@ -18,9 +18,40 @@ def read_cate_to_db():
     :return:
     """
     cate_url = "https://www.jd.com/allSort.aspx"
-    cate_response = requests.get(cate_url)
+    cate_response = get_http_response(cate_url)
     if cate_response.status_code == 200:
-        print('success')
+        cate_values = []
+        # html.parser, lxml, xml, html5lib
+		bs_obj = bs(cate_response.text, 'html.parser')
+        box_list = bs_obj.select('div.category-item')
+        for box in box_list:
+            root_span = box.select_one("span")
+            print(root_span)
+            if root_span is None or root_span.text != "手机":
+                continue
+            print(box)
+            root_name = root_span.text
+            dl_list = box.select("dl.clearfix")
+            for dl in dl_list:
+                link_list = dl.select("a")
+                if len(link_list) == 0:
+                    continue
+                second_name = link_list[0].text
+                for link in link_list[1:]:
+                    url = link.attrs["href"]
+                    third_name = link.text
+                    cid = int(url[url.rfind(",") + 1:])
+                    cate_values.append({"cid": cid,
+										"root_name": root_name,
+										"second_name": second_name,
+										"cname": third_name,
+										"curl": "https:" + url})
+        if len(cate_values) > 0:
+            sqlite_conn = SqliteHelper()
+            sqlite_conn.exec_many_no_query("INSERT INTO %s(cid,root_name,second_name,cname,curl) " % cate_tb_name +
+										   "VALUES(:cid, :root_name, :second_name, :cname, :curl)", cate_values)
+        print("read cate over")
+        return
     print("cate read error,code=", cate_response.status_code)
 
 
---
+++ readme.txt
@@ -0,0 +1,12 @@
+必要文件为
+config.py   数据库配置
+sqlHelper.py 数据库操做
+cate.py  京东数据抓取
+
+
+需要python版本为python 3.5 (开发环境为3.5.2）
+
+jdpyscript.txt 为开发环境安装过的包
+
+pip install -r jdpyscript.txt  可已安装这些包
+
